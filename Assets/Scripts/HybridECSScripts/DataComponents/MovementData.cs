b54b696ec40df9c004d5f6e6232f8d635d0af087
Michael Prommer <michael.prommer@outlook.de>
2021-01-12 13:31:05 +0100

    Erster ECS Versuch (Complete Fail)

---
+++ Assets/Scripts/HybridECSScripts/DataComponents/MovementData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct MovementData : IComponentData
+{
+    public float3 velocity;
+    public float speed;
+}
---
+++ Assets/Scripts/HybridECSScripts/Systems/MovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class MovementSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((ref Translation translation, ref Rotation rotation, ref MovementData movementData) =>
+        {
+            rotation.Value = Quaternion.Slerp(rotation.Value, Quaternion.LookRotation(movementData.velocity), 0.15f);
+            translation.Value += movementData.velocity * (movementData.speed * deltaTime);
+        });
+    }
+}
